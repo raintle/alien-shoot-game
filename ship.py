a8e1ff41ba4c300ff1e4293e8cd86a0286331424
raintle <2162951845@qq.com>
2026-01-20 09:00:42 +0800

    add

---
+++ ship.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Ship:
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image_load = pygame.image.load('早露.png')
+        #图片导入
+        self.image_surf = pygame.transform.scale_by(self.image_load, 0.2)
+        #图片缩放
+        self.image = self.image_surf
+        #赋值
+        self.rect = self.image.get_rect()
+        #获取图片中央坐标
+
+        self.rect.midbottom = self.screen_rect.midbottom
+        #
+
+    def blitme(self):
+
+        self.screen.blit(self.image, self.rect)
